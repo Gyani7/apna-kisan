77f17909a32256d7e0701d0fbda8a66013aa9f25
Gyani7 <shubhashchaturvedi92@gmail.com>
2026-06-13 14:49:19 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,6 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
-
-  const sensitiveParams = ['aadhaar', 'pan', 'govtId'];
-
-  for (const param of sensitiveParams) {
-    if (url.searchParams.has(param)) {
-      url.searchParams.set(param, `[ID Masked: ${param.toUpperCase()}]`);
-    }
-  }
-
   return NextResponse.next();
 }
 
