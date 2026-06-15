a3b6ee3cd9c3b241e9d2b7d2c84117a8337f0499
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-06-15 07:20:03 +0000

    fix: vercel build by creating empty types file

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -1,2 +1,21 @@
-Invalid project ref format. Must be like `abcdefghijklmnopqrst`.
-Try rerunning the command with --debug to troubleshoot the error.
+export type Json = | string | number | boolean | null | { [key: string]: Json | undefined } | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      [_ in never]: never
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
