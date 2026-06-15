0b845378fbbe659b5f6e3dd6af5a41451a20a21d
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-06-15 10:50:59 +0000

    fix: add logo icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,7 +1,8 @@
-import { ChevronLeft, LucideProps, Moon, SunMedium, type Icon as LucideIcon, } from 'lucide-react';
+import { ChevronLeft, Command, LucideProps, Moon, SunMedium, type Icon as LucideIcon, } from 'lucide-react';
 
 export const Icons = {
     Sun: SunMedium,
     Moon: Moon,
     ChevronLeft: ChevronLeft,
+    logo: Command,
 };
