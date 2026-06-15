fcec1e75b61898459ab5a25ea77639a7f3bed7e9
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-06-15 11:41:33 +0000

    fix: remove unused MapPin import

--- components/StoryCard.tsx
+++ components/StoryCard.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { Clock, MapPin, BookOpen } from 'lucide-react';
+import { Clock, BookOpen } from 'lucide-react';
 import { PostWithAuthor, timeAgo, POST_TYPE_CONFIG } from '@/lib/types';
 
 interface StoryCardProps {
