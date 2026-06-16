5d65ffc3fd37af3da61f97d6da028db2f1bfd36e
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-06-16 12:28:10 +0000

    revert: revert to dynamic button variants

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { buttonVariants } from "@/components/ui/button";
 import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export default function Home() {
@@ -19,7 +21,7 @@ export default function Home() {
           href={siteConfig.links.docs}
           target="_blank"
           rel="noreferrer"
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 bg-primary text-primary-foreground hover:bg-primary/90"
+          className={cn(buttonVariants())}
         >
           Documentation
         </Link>
@@ -27,7 +29,7 @@ export default function Home() {
           target="_blank"
           rel="noreferrer"
           href={siteConfig.links.github}
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 border border-input bg-background hover:bg-accent hover:text-accent-foreground"
+          className={cn(buttonVariants({ variant: "outline" }))}
         >
           GitHub
         </Link>
