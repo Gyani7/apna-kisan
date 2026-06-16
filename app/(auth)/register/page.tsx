b6b468eec061aaca1623c18f0087660ad7a9de29
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-06-16 10:06:23 +0000

    fix: Correct casing for UserAuthForm import

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { UserAuthForm } from "@/components/user-auth-form"
+import { UserAuthForm } from "@/components/UserAuthForm"
 
 export const metadata: Metadata = {
   title: "Create an account",
