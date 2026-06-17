6a3bfee9a1a12a7b760c4a8704b1aa06e9b36ebf
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-06-17 10:45:17 +0000

    fix: Uncomment WeatherCard to isolate error

--- app/(main)/dashboard/farmer/page.tsx
+++ app/(main)/dashboard/farmer/page.tsx
@@ -107,7 +107,7 @@ export default function FarmerDashboard() {
         
         {/* Existing Cards - can be redesigned or used as is */}
         <div className="col-span-1 lg:col-span-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {/* <WeatherCard /> */}
+            <WeatherCard />
             {/* <MandiRatesCard /> */}
             {/* <CropInsightsCard /> */}
         </div>
