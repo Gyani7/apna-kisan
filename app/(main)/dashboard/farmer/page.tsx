036dd5cef987a67592c498d3c63fb9940eaddf50
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-06-17 10:59:49 +0000

    fix: Add 'use client' to WeatherCard and restore FarmerDashboard

--- app/(main)/dashboard/farmer/page.tsx
+++ app/(main)/dashboard/farmer/page.tsx
@@ -108,8 +108,8 @@ export default function FarmerDashboard() {
         {/* Existing Cards - can be redesigned or used as is */}
         <div className="col-span-1 lg:col-span-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <WeatherCard />
-            {/* <MandiRatesCard /> */}
-            {/* <CropInsightsCard /> */}
+            <MandiRatesCard />
+            <CropInsightsCard />
         </div>
       </div>
     </div>
