d3fd5b27d397d6b506310ed41ed06f39e8cb8377
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-06-18 12:37:13 +0000

    fix: resolve conflicting routes

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
-
-/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
 
-const nextConfig = withPWA({
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
+module.exports = withPWA({
+  // experimental: {
+  //   appDir: true,
+  // },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 });
-
-module.exports = nextConfig;
