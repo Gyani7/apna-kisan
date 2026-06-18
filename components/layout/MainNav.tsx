60311cbc2630a34c9251095918a8a8c97a96260e
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-06-18 12:44:28 +0000

    fix: resolve build errors

--- components/layout/MainNav.tsx
+++ components/layout/MainNav.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
-import { Icons } from "@/components/Icons";
+import { Icons } from "../icons";
 import { MainNavItem } from "@/types";
 
 interface MainNavProps {
