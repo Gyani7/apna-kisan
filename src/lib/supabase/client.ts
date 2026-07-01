a1226d283dd804a9b38a27abd41eff236d3fe261
shubhashchaturvedi92 <shubhashchaturvedi92@gmail.com>
2026-07-01 13:10:02 +0000

    fix: migrate to @supabase/ssr

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,4 +1,4 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createBrowserClient } from '@supabase/ssr';
 import { Database } from '@/lib/database.types';
 
 export const createSupabaseClient = () => createBrowserClient<Database>(
